b2e04865887fb38b8472c3b909e8e658204598fb
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2022-03-04 12:50:12 +0700

    added methods for scanning operator, identifier and number

--- arith-simplifier/src/com/github/artfly/simplifier/Lexer.java
+++ arith-simplifier/src/com/github/artfly/simplifier/Lexer.java
@@ -4,7 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+/*
+TODO:
+1. i incremented incorrectly
+2. error handling
+3. number token has incorrect value type
+ */
 public class Lexer {
 
     private final BufferedReader br;
@@ -30,14 +37,25 @@ public class Lexer {
                     i = identifier(i, line);
                     continue;
                 }
-                if (isOperator(c)) {
-                    i = operator(i, line);
+                TokenType tokenType = switch (c) {
+                    case '^' -> TokenType.POW;
+                    case '/' -> TokenType.DIVIDE;
+                    case '*' -> TokenType.MULTIPLY;
+                    case '+' -> TokenType.PLUS;
+                    case '-' -> TokenType.MINUS;
+                    case '(' -> TokenType.OP_BRACE;
+                    case ')' -> TokenType.CL_BRACE;
+                    default -> null;
+                };
+                if (tokenType != null) {
+                    tokens.add(new Token(tokenType, -1, c));
+                    i = i + 1;
                     continue;
                 }
                 System.err.println("Unknown char: " + c);
             }
         }
-        return null;
+        return tokens;
     }
 
     private String getLine() {
@@ -48,35 +66,30 @@ public class Lexer {
         }
     }
 
-    private int operator(int i, String line) {
-        // TODO
-        return i;
-    }
-
-    private boolean isOperator(char c) {
-        // TODO
-        return false;
-    }
-
     private int identifier(int i, String line) {
-        // TODO
-        return i;
+        return scanToken(i, line, TokenType.IDENTIFIER, Lexer::isLetterOrDigit);
     }
 
     // 1(
     private int number(int i, String line) {
-        StringBuilder sb = new StringBuilder();
+        return scanToken(i, line, TokenType.NUMBER, Lexer::isDigit);
+    }
+
+    private int scanToken(int i, String line, TokenType tokenType, Predicate<Character> condition) {
+        int start = i;
         do {
-            sb.append(line.charAt(i));
             i++;
-        } while (i != line.length() && Character.isDigit(line.charAt(i)));
-        String value = sb.toString();
-        tokens.add(new Token(TokenType.NUMBER, -1, value));
+        } while (i != line.length() && condition.test(line.charAt(i)));
+        String value = line.substring(start, i);
+        tokens.add(new Token(tokenType, -1, value));
         return i;
     }
 
+    private static boolean isLetterOrDigit(char c) {
+        return isDigit(c) || Character.isLetter(c);
+    }
 
-    private boolean isDigit(char c) {
+    private static boolean isDigit(char c) {
         return c >= '0' && c <= '9';
     }
 }
